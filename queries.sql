2797875ecf556c451264f75917723e1c231aa9af
Julian Amparan <amparanjulian01@gmail.com>
2022-12-09 13:09:25 -0600

    query tests fix.

--- queries.sql
+++ queries.sql
@@ -1,21 +1,23 @@
 -- Part 1: Test it with SQL
-/* Two columns;
-id = int (not nullable);
-employer,
-name,
-skills = varchar(255) (nullable)*/
+
+-- Two columns;
+-- id = int (not nullable);
+-- employer,
+-- name,
+-- skills = varchar(255) (nullable)
 SELECT COLUMN_NAME, DATA_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE
 	TABLE_SCHEMA = 'techjobs' AND
     TABLE_NAME = 'job'
 -- Part 2: Test it with SQL
-SELECT name FROM employer WHERE location = 'St. Louis City';
+SELECT name FROM employer WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
-DROP TABLE techjobs.job
+DROP TABLE job;
 -- Part 4: Test it with SQL
-SELECT techjobs.skill.name
-FROM techjobs.job_skills
-INNER JOIN techjobs.skill
-ON techjobs.skill.id = techjobs.job_skills.skills_id
-ORDER BY techjobs.skill.name DESC;
+SELECT *
+FROM skill
+INNER JOIN job_skills
+ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
